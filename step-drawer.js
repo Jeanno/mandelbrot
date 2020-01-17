1b473aa792e9c55d100cd391270fa7ffeca90327
Jeanno <jeanno.cheung@gmail.com>
2020-01-17 18:06:20 -0500

    More optimizations - Increase locality with repeating inner loop - Use color cache to avoid repeat color calculations

--- step-drawer.js
+++ step-drawer.js
@@ -7,6 +7,8 @@ class StepDrawer {
         this._maxIter = 100;
         this._zs = new Array(canvas.height * canvas.width);
         this.initZ();
+
+        this._colorCache = [];
     }
 
     getZ(x, y) {
@@ -41,12 +43,17 @@ class StepDrawer {
         this.clearZ();
         mc.ctx.fillStyle = "#000";
         mc.ctx.fillRect(0, 0, width, height);
-        this.drawStep(0);
+
+        var that = this;
+        this._timeout = setTimeout(function() {
+            that.drawStep(0);
+        }, 0);
     }
 
     drawStep(iter) {
         var mc = this._mandelbrotCanvas;
-        var multi = 8;
+        // Use multi inner loop to increase locality
+        var multi = Math.min(iter + 1, 128);
         var top = mc.top;
         var left = mc.left;
         var step = mc.step;
@@ -65,8 +72,8 @@ class StepDrawer {
                     var ysq = z[1] * z[1];
                     if (xsq + ysq > threshold) {
                         z[2] = true;
-                        this._maxIter = Math.max(this._maxIter, (iter + k) * 1.1);
                         this.drawPx(i, j, iter + k);
+                        this._maxIter = Math.max(this._maxIter, (iter * 1.04)|0);
                     }
                     z[1] = 2 * z[0] * z[1] + y0;
                     z[0] = xsq - ysq + x0;
@@ -85,20 +92,28 @@ class StepDrawer {
 
     }
 
+    getColorForStep(step) {
+        if (step === -1) {
+            return "#000";
+        } else {
+            var ret = this._colorCache[step];
+            if (!ret) {
+                var denominator = 1000;
+                var frac_sq = Math.pow(step, 1/2.5) / Math.pow(denominator, 1/2.5);
+                var frac = step / denominator;
+                var h = 241.1 * (1 - frac_sq) + 37 * frac_sq;
+                var s = 100 * (1 - frac * (1 - frac));
+                var l = 100 * (0.1 + frac_sq) / 1.1;
+
+                ret = this._colorCache[step] = 'hsl(' + h + ',' + s + '%,' + l + '%)';
+            }
+            return ret;
+        }
+    }
+
     drawPx(x, y, step) {
         var mc = this._mandelbrotCanvas;
-        if (step === -1) {
-            mc.ctx.fillStyle = "#000000";
-        } else {
-            var denominator = 1000;
-            var frac_sq = Math.pow(step, 1/2.5) / Math.pow(denominator, 1/2.5);
-            var frac = step / denominator;
-            var h = 241.1 * (1 - frac_sq) + 37 * frac_sq;
-            var s = 100 * (1 - frac * (1 - frac));
-            var l = 100 * (0.1 + frac_sq) / 1.1;
-
-            mc.ctx.fillStyle = 'hsl(' + h + ',' + s + '%,' + l + '%)';
-        }
+        mc.ctx.fillStyle = this.getColorForStep(step);
         mc.ctx.fillRect(x, y, 1, 1);
     }
 }
